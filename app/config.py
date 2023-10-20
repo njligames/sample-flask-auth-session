de0e124951d137885b5b4d6bb0f28f1dcf01f196
James Folk <jamesfolk1@gmail.com>
2023-10-20 15:54:56 -0400

    james folk first

--- app/config.py
+++ app/config.py
@@ -12,9 +12,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
 
     CSRF_ENABLED = True
-	
+
     # Set up the App SECRET_KEY
-    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
+    SECRET_KEY = config('SECRET_KEY', default=os.environ["SECRET_KEY"])
 
     # This will create a file in <app> FOLDER
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
